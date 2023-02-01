800820695e23d2f26da739d5210cbbf5b663b1da
Danilo Heraclio <emaildedanilo@gmail.com>
2023-02-01 16:21:24 -0500

    web add animations

--- web/src/components/HabitsList.tsx
+++ web/src/components/HabitsList.tsx
@@ -63,9 +63,9 @@ export function HabitsList({ date, onCompletedChanged }: HabitsListProps) {
             checked={habitsInfo?.completedHabits?.includes(habit.id)}
             onCheckedChange={() => handleToggleHabbit(habit.id)}
             disabled={isDateInPast}
-            className="flex items-center gap-3 group"
+            className="flex items-center gap-3 group focus:outline-none disabled:cursor-not-allowed"
           >
-            <div className="w-8 h-8 flex items-center justify-center rounded-lg bg-zinc-900 border-2 border-zinc-800 group-data-[state=checked]:bg-green-500 group-data-[state=checked]:border-green-500">
+            <div className="w-8 h-8 flex items-center justify-center rounded-lg bg-zinc-900 border-2 border-zinc-800 group-data-[state=checked]:bg-green-500 group-data-[state=checked]:border-green-500 group-focus:ring-2 group-focus:ring-violet-700 group-focus:ring-offset-2 group-focus:ring-offset-background">
               <Checkbox.Indicator>
                 <Check size={20} className="text-white" />
               </Checkbox.Indicator>
